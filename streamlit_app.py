52050dbe4cea311d74bd8b251a2cdd77e9b56e4a
eatmeimadanish <eatmeimadanish@gmail.com>
2024-08-08 08:42:01 -0500

    Enhance streamlit app for nurse metrics and acuity charts
    Updated the streamlit app to include additional acuity metrics from the snowflake API and improved sorting mechanism. Adjusted the requirements.txt to use the latest version of urllib3. Added new functions and columns for handling and displaying the acuity and other nurse-related metrics.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
-from nfty.sflake import API as sflake_API, report_dict
+from nfty.sflake import API as sflake_API, report_dict, d_cols, create_month_year_index
 from pandas.tseries.offsets import DateOffset
 import arrow
 
@@ -10,8 +10,8 @@ import arrow
 def load_report(report='patients_seen'):
     s = sflake_API()
     if report == 'charts':
-        nurses, nonurses = s.charts()
-        return pd.json_normalize(nurses), pd.json_normalize(nonurses)
+        x = s.charts()
+        return pd.json_normalize(x[0]), pd.json_normalize(x[1]), pd.json_normalize(x[2])
     return pd.json_normalize(s.report(report))
 
 
@@ -39,7 +39,11 @@ def app():
         df = load_report(report_dict[report_select])
         gb = GridOptionsBuilder.from_dataframe(df)
         gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
-        gb.configure_column('HOURS', type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'], precision=2)
+        for d in d_cols:
+            if d in df.columns:
+                gb.configure_column(d, type=['numericColumn', 'numberColumnFilter', 'customNumericFormat'], precision=2)
+        if 'YEAR' in df.columns:
+            df.sort_values(by=['YEAR', 'MONTH'], ascending=[False, False], inplace=True)
 
         # In Case you want to autosize the columns instead
         if params.get('resize'):
@@ -69,19 +73,24 @@ def app():
                # This should be your actual ag-grid license key
                )
     else:
-        nurses, nonurses = load_report('charts')
+        nurses, nonurses, acuity = load_report('charts')
         month_map = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
                      7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+        mvals = list(month_map.values())
+        month_mmm_map = create_month_year_index()
         # nurses = nurses[nurses['DISCIPLINE'].isin(['RN', 'LVN'])].groupby(nurses.columns.drop('DISCIPLINE').tolist()).size().reset_index().rename(columns={0: 'count'})
         billed = nurses.melt(id_vars='MONTH', value_vars=['HOURS_THIS_YEAR', 'HOURS_LAST_YEAR'])
         ot = nurses.melt(id_vars='MONTH', value_vars=['OT_%_THIS_YEAR', 'OT_%_LAST_YEAR'])
         unbilled = nurses.melt(id_vars='MONTH', value_vars=['UN_BILLED_THIS_YEAR'])
+        acuity_compared = acuity.melt(id_vars='MONTH_ABBR', value_vars=['HIGH_ACUITY_%', 'PRIOR_YEAR_%'])
         billed['MONTH'] = billed['MONTH'].map(month_map)
         ot['MONTH'] = ot['MONTH'].map(month_map)
-        chart = alt.Chart(billed).encode(x=alt.X('MONTH:O', sort=list(month_map.values()), title=None), y=alt.Y('value', title='Nurse Billable Hours'), color='variable').mark_line()
-        chart += alt.Chart(billed).mark_point().encode(x=alt.X('MONTH:O', sort=list(month_map.values()), title=None), y=alt.Y('value', title='Nurse Billable Hours'), color='variable')
-        chart2 = alt.Chart(ot).encode(x=alt.X('MONTH:O', sort=list(month_map.values()), title=None), y=alt.Y('value', title='Nurse OT %'), color='variable').mark_line()
-        chart2 += alt.Chart(ot).mark_point().encode(x=alt.X('MONTH:O', sort=list(month_map.values()), title=None), y=alt.Y('value', title='Nurse OT %'), color='variable')
+        chart = alt.Chart(billed).encode(x=alt.X('MONTH:O', sort=mvals, title=None), y=alt.Y('value', title='Nurse Billable Hours'), color='variable').mark_line()
+        chart += alt.Chart(billed).mark_point().encode(x=alt.X('MONTH:O', sort=mvals, title=None), y=alt.Y('value', title='Nurse Billable Hours'), color='variable')
+        chart2 = alt.Chart(ot).encode(x=alt.X('MONTH:O', sort=mvals, title=None), y=alt.Y('value', title='Nurse OT %'), color='variable').mark_line()
+        chart2 += alt.Chart(ot).mark_point().encode(x=alt.X('MONTH:O', sort=mvals, title=None), y=alt.Y('value', title='Nurse OT %'), color='variable')
+        acuity_chart = alt.Chart(acuity_compared).encode(x=alt.X('MONTH_ABBR:O', sort=mvals, title=None), y=alt.Y('value', title='Acuity %'), color='variable').mark_line()
+        acuity_chart += alt.Chart(acuity_compared).mark_point().encode(x=alt.X('MONTH_ABBR:O', sort=mvals, title=None), y=alt.Y('value', title='Acuity %'), color='variable')
         current_month = pd.to_datetime('today').month
         last_month = current_month - 1 if current_month != 1 else 12
         unbilled = unbilled[unbilled['MONTH'] <= last_month]
@@ -91,7 +100,7 @@ def app():
             y=alt.Y('value', title='Unbilled Hours'))
         bar += alt.Chart(unbilled).mark_rule(color='red').encode(y=alt.Y('mean(value):Q', title='Unbilled Hours'))
         st.altair_chart(chart | chart2 | bar, use_container_width=True)
-        # st.altair_chart(bar, use_container_width=True)
+        st.altair_chart(acuity_chart, use_container_width=True)
 
 if __name__ == '__main__':
     app()
